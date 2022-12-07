650b74a683aa06c6230def2ccf8864d52c4434db
hinatha <hinatha0110@gmail.com>
2022-12-07 14:14:15 +0900

    custom hooksにTODO作成機能を追加

--- src/hooks/useTodoList.ts
+++ src/hooks/useTodoList.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { Todo, Params } from './types';
 
 // It's mock
@@ -34,7 +34,36 @@ const useTodoList = () => {
   // Set useState and first value is mockTodo
   const [todos, setTodos] = useState<Todo[]>(mockTodo);
 
-  return { todos };
+  // Add Todo logic
+  // Memoization by useCallback
+  const addTodo = useCallback(
+    (params: Params) => {
+      // Change type from Param to Todo
+      const todo = intitializeTodo(params)
+      // Generate new todo list to change state
+      const newTodos = [...todos];
+      // Add todo to todo list
+      newTodos.push(todo);
+      setTodos(newTodos);
+    },
+    // Set todos as dependent array
+    [todos]
+  );
+
+  // Function of changing param to Todo
+  const intitializeTodo = (todo: Params) =>  {
+    const date = new Date()
+    return {
+      id: date.getTime(),
+      title: todo.title,
+      description: todo.description,
+      status: todo.status,
+      createdAt: date,
+      updatedAt: date,
+    } as Todo
+  }
+
+  return { todos, addTodo };
 }
 
 export default useTodoList;
--- src/router/RouterConfig.tsx
+++ src/router/RouterConfig.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Todo from "../components/Todo";
 import AddTodo from "../components/AddTodo";
