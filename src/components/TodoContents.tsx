85f28587265ad94d731de3e85220cb3fcea32c2d
hinatha <hinatha0110@gmail.com>
2022-12-13 16:27:10 +0900

    [W[WIP]Restore function of showing todo list

---
+++ src/components/TodoContents.tsx
@@ -0,0 +1,18 @@
+
+const style = {
+    marginBottom: "20px",
+    border: "1px solid",
+    boxShadow: "2px 2px 4px gray",
+    width: "250px",
+}
+
+const TodoCard = () => {
+  
+    return (
+        <div style={ style }>
+            
+        </div>
+    );
+  };
+  
+  export default TodoCard;
