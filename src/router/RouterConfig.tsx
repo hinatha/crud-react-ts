dc78c5215eeb507bbde9bc1e24d6b4bcd0e71e20
hinatha <hinatha0110@gmail.com>
2022-12-20 14:52:51 +0900

    RouterにEditTodo page追加

--- src/router/RouterConfig.tsx
+++ src/router/RouterConfig.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../components/App";
 import AddTodo from "../components/AddTodo";
+import EditTodo from "../components/EditTodo";
 
 const RouterConfig = () => {
   return (
@@ -8,6 +9,7 @@ const RouterConfig = () => {
       <Routes>
         <Route index element={<App />} />
         <Route path="new" element={<AddTodo />} />
+        <Route path="/edit/:id" element={<EditTodo />} />
       </Routes>
     </BrowserRouter>
   );
